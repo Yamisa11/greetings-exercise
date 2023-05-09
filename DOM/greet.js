8554f4236ca06b31c9edb761608ec0ac4d8cd679
Siyabonga <c.ya@live.com>
2023-05-09 16:01:54 +0200

    update

--- DOM/greet.js
+++ DOM/greet.js
@@ -3,6 +3,7 @@ var smtButton = document.querySelector(".submit");
 var displaySpan = document.querySelector("#display")
 var displayNumber = document.querySelector("#displayNumber")
 var inputText = document.querySelector(".name")
+
 var totalNames = 0;
 
 smtButton.addEventListener("click", function(){
@@ -22,11 +23,12 @@ smtButton.addEventListener("click", function(){
     if (checkedRadio.value === "isiZulu") {
         displaySpan.innerHTML = greetingFunction.isiZuluGreeting()
     }
+    var arrayOfNames = greetingFunction.allNamesFunction();
     
-    localStorage["names"] = greetingFunction.allNamesFunction()
+    localStorage["names"] = arrayOfNames
 
-    for (let i = 0; i < greetingFunction.allNamesFunction.length; i++) {
-       totalNames = greetingFunction.countNames()
+    for (let i = 0; i < arrayOfNames.length; i++) {
+       totalNames++
     }
 
     displayNumber.innerHTML = totalNames;
